e9f293de3e078b37b3668c9b17c7946799eeab3c
Diogo Kollross <diogoko@gmail.com>
2016-07-10 10:51:17 -0300

    Documentation

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # bdd-proxy
+
+Proxy for calling methods using BDD style.
+
+## Usage
+
+```php
+class MyTestCase extends \PHPUnit\Framework\TestCase {
+  function setUp() {
+    $this->bdd = new \diogoko\bdd\BDDProxy($this, 'given|when|then', 'and');
+  }
+
+  function testSum() {
+    $this->bdd->given('that a is number', 3)
+        ->and('that b is number', 5)
+        ->when('adding a and b')
+        ->then('the result is', 8);
+  }
+
+  /**
+   * @given that a is number
+   */
+  function that_a_is_number($x) {
+    $this->a = $x;
+  }
+
+  /**
+   * @given that b is number
+   */
+  function that_b_is_number($x) {
+    $this->b = $x;
+  }
+
+  /**
+   * @when adding a and b
+   */
+  function adding_a_and_b($x) {
+    $this->result = $this->a + $this->b;
+  }
+
+  /**
+   * @then the result is
+   */
+  function the_result_is($result) {
+    $this->assertEquals($result, $this->result);
+  }
+}
+```
--- src/BDDProxy.php
+++ src/BDDProxy.php
@@ -3,38 +3,99 @@
 namespace diogoko\bdd;
 
 
+/**
+ * Proxy for calling methods using BDD style.
+ */
 class BDDProxy {
+  /**
+   * The proxied object.
+   * 
+   * @var object
+   */
   private $target;
   
+  /**
+   * An associative array that maps a step kind and description to a method.
+   * 
+   * @var array
+   */
   private $stepMap;
   
+  /**
+   * The name of the current step kind, if any.
+   * 
+   * @var string
+   */
   private $currentStepKind;
   
-  private $expectedKinds;
+  /**
+   * A regular expression fragment that matches all step kinds.
+   * 
+   * @var string
+   */
+  private $stepsPattern;
   
-  private $continuationKind;
+  /**
+   * A regular expression fragment that matches continuation of steps.
+   * 
+   * @var string
+   */
+  private $continuationPattern;
   
-  public function __construct($target, $expectedKinds, $continuationKind) {
+  /**
+   * Configures a new proxy.
+   * 
+   * @param object $target The proxied object
+   * @param string $stepsPattern A regular expression fragment that matches all 
+   *                             step kinds
+   * @param string $continuationPattern A regular expression fragment that 
+   *                                    matches continuations of steps
+   * 
+   * @throws \InvalidArgumentException If two methods are registered with the 
+   *                                   same kind and description
+   */
+  public function __construct($target, $stepsPattern, $continuationPattern) {
     $this->target = $target;
-    $this->expectedKinds = $expectedKinds;
-    $this->continuationKind = $continuationKind;
+    $this->stepsPattern = $stepsPattern;
+    $this->continuationPattern = $continuationPattern;
     $this->buildStepMap();
   }
   
+  /**
+   * @copydoc ::delegateCall
+   */
   public function __call($name, $arguments) {
     return $this->delegateCall($name, $arguments);
   }
   
+  /**
+   * Calls a method in the target object using BDD style.
+   * 
+   * @param string $name The method name
+   * @param array $arguments All the method arguments
+   * @return BDDProxy This object
+   */
   public function delegateCall($name, $arguments) {
     // TODO: validate args
     return $this->invokeStep($name, $arguments[0], array_slice($arguments, 1));
   }
   
-  public function invokeStep($kind, $description, $arguments) {
-    $isContinuation = (preg_match('/^' . $this->continuationKind . '$/', $kind));
+  /**
+   * Calls the method corresponding to a given step kind and description.
+   * 
+   * @param string $kind The step kind
+   * @param string $description The step description
+   * @param array $arguments The step arguments
+   * @return \diogoko\bdd\BDDProxy This object
+   * @throws \LogicException If a continuation is called before a step method
+   * @throws \BadMethodCallException If no method is found with the given step 
+   *                                 kind and description
+   */
+  private function invokeStep($kind, $description, $arguments) {
+    $isContinuation = (preg_match('/^' . $this->continuationPattern . '$/', $kind));
     if ($isContinuation) {
       if (!$this->currentStepKind) {
-        throw new \LogicException("Continuation method ({$kind}) must be called after step method ({$this->expectedKinds})");
+        throw new \LogicException("Continuation method ({$kind}) must be called after step method ({$this->stepsPattern})");
       }
     } else {
       $this->currentStepKind = $kind;
@@ -51,6 +112,12 @@ class BDDProxy {
     }
   }
   
+  /**
+   * Builds the map from step kind and description to method.
+   * 
+   * @throws \InvalidArgumentException If two methods are registered with the 
+   *                                   same kind and description
+   */
   private function buildStepMap() {
     $this->stepMap = [];
     
@@ -60,10 +127,19 @@ class BDDProxy {
     }
   }
   
+  /**
+   * Searches all method descriptions in annotations that match the steps 
+   * pattern.
+   * 
+   * @param \ReflectionMethod $method The method whose descriptions will be 
+   *                                  registered
+   * @throws \InvalidArgumentException If two methods are registered with the 
+   *                                   same kind and description
+   */
   private function registerMethodDescriptions($method) {
     $docblock = $method->getDocComment();
     
-    $regexp = '/@(' . $this->expectedKinds . '|' . $this->continuationKind . ')\s+(.+)\r?\n/';
+    $regexp = '/@(' . $this->stepsPattern . ')\s+(.+)\r?\n/';
     if (!preg_match_all($regexp, $docblock, $matches, PREG_SET_ORDER)) {
       return;
     }
@@ -73,6 +149,15 @@ class BDDProxy {
     }
   }
   
+  /**
+   * Registers a method to the given step kind and description.
+   * 
+   * @param \ReflectionMethod $method The registered method
+   * @param string $kind The step kind
+   * @param string $description The step description
+   * @throws \InvalidArgumentException If two methods are registered with the 
+   *                                   same kind and description
+   */
   private function registerMethod($method, $kind, $description) {
     if (isset($this->stepMap[$kind][$description])) {
       $className = $method->getDeclaringClass()->getName();
